28d8be0d4070e66e28f1bbab152c69164e92ee1f
Yash <yashchaurasia630@gmail.com>
2024-01-22 17:39:25 +0530

    added auto dismissing alerts

--- textUtils/src/App.js
+++ textUtils/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 // import About from "./components/About";
 import Navbar from './components/Navbar'
 import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -11,17 +12,33 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor = '#212529'
       document.body.style.color = 'white'
+      showAlert('Dark mode enabled!', 'success')
     }
     else {
       setMode('light')
       document.body.style.backgroundColor = 'white'
       document.body.style.color = 'black'
+      showAlert('Dark mode disabled!', 'success')
     }
+  }
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, alertType) => {
+    setAlert({
+      message: message,
+      type: alertType
+    });
+
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
   }
 
   return (
     <>
       <Navbar title='random bullshit go' mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
         <TextForm heading="Enter Text" />
       </div>
--- textUtils/src/components/TextForm.js
+++ textUtils/src/components/TextForm.js
@@ -16,11 +16,11 @@ export default function TextForm(props) {
         <>
             <div className="container">
                 <h1>{props.heading}</h1>
-                <div className={`mb-3`}>
+                <div className='mb-3'>
                     <textarea className="form-control" id="myBox" rows="8" placeholder="Random bullshit text" value={text} onChange={handleOnChange}></textarea>
                 </div>
                 <button className="btn btn-primary" onClick={handleUpClick}>click and see</button>
-            </div>
+            </div >
             <div className="container my-3">
                 <h2>Your text Summary</h2>
                 <p>{text.trim().split(' ').length} words and {text.length} characters</p>
