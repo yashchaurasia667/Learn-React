fb102c4db75d81483d3569cd49bec7785a613169
Yash <yashchaurasia630@gmail.com>
2024-06-24 21:07:28 +0530

    started a one shot

--- class-based-components/src/components/News.js
+++ class-based-components/src/components/News.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  constructor() {
+    super();
+  }
   render() {
     return (
-      <div>
-        news comp
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className='container my-3 '>
+        <h2>NewsMonkey Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="Title" description="This is a long as fuck description. This is very important you should read this" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="Title" description="This is a long as fuck description. This is very important you should read this" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="Title" description="This is a long as fuck description. This is very important you should read this" />
+          </div>
+        </div>
       </div>
     )
   }
--- class-based-components/src/components/NewsItem.js
+++ class-based-components/src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description, imageUrl} = this.props;
     return (
-      <div>
-        This is a news item
+      <div className='my-3'>
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-primary btn-sm">Read more..</a>
+          </div>
+        </div>
       </div>
     )
   }
