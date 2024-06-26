f63a95c8375e83d9db3ea0ef99bd8c561512b725
Yash <yashchaurasia630@gmail.com>
2024-06-26 22:12:18 +0530

    added more routes with react router

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout></MainLayout>}>
+      <Route index element={<HomePage />}></Route>
+      <Route path="/jobs" element={<JobsPage />}></Route>
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar></Navbar>
-      <Hero
-        title="Become a React Dev"
-        subtitle="Find the React job that fits your skill set"
-      ></Hero>
-      <HomeCards></HomeCards>
-      <JobListings></JobListings>
-      <ViewAllJobs></ViewAllJobs>
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ react-crash-course/src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import HomeCards from '../components/HomeCards';
+import JobListings from '../components/JobListings';
+import ViewAllJobs from '../components/ViewAllJobs';
+
+const HomePage = () => {
+  return (
+    <>
+    <Hero title="Become a React Dev" subtitle="Find the React job that fits your skill set"></Hero>
+    <HomeCards />
+    <JobListings />
+    <ViewAllJobs />
+    </>
+    )
+}
+
+export default HomePage
