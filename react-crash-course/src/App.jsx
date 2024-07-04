d2892c933d274c46602a6d9605e988105653f1a6
Yash <yashchaurasia630@gmail.com>
2024-07-04 15:04:51 +0530

    fixed errors and made the job page dynamic and added a proxy for localhost

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -9,13 +9,15 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFound from "./pages/NotFound";
+import JobPage, { jobLoader } from "./pages/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout></MainLayout>}>
-      <Route index element={<HomePage />}></Route>
-      <Route path="/jobs" element={<JobsPage />}></Route>
-      <Route path="*" element={<NotFound />}></Route>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
--- react-crash-course/src/components/JobListings.jsx
+++ react-crash-course/src/components/JobListings.jsx
@@ -11,7 +11,7 @@ const JobListings = ({ isHome = false }) => {
     const fetchJobs = async () => {
       const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
       try {
-        const res = await fetch({apiUrl});
+        const res = await fetch(apiUrl);
         const data = await res.json();
         setJobs(data);
       } catch (error) {
